ec00c047246ebaaf0e650b474a3f89757f4e44ac
AthulKrishna14310 <athul.krishna14310@gmail.com>
2019-08-17 00:36:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/concentriccircledraw/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.concentriccircledraw;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+import com.crowdfire.cfalertdialog.CFAlertDialog;
+
+public class MainActivity extends AppCompatActivity {
+    private Button finishButton;
+    private PaintView paintView;
+    private Button    refreshButton;
+    private Chronometer chronometer;
+    private long base;
+    private boolean start=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        finishButton=(Button)findViewById(R.id.finishButton);
+        paintView=(PaintView)findViewById(R.id.paintView);
+        refreshButton=(Button)findViewById(R.id.refreshButton);
+        chronometer=(Chronometer)findViewById(R.id.chronometer);
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                long time= SystemClock.elapsedRealtime()-chronometer.getBase();
+                float x=paintView.getTouched()/(time/1000);
+                int marks=(int)x;
+                finishButton.setText("Score:"+marks);
+                showfinalDialogue(marks,paintView);
+                 }
+        });
+
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        });
+
+        paintView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                   if(start) {
+                       chronometer.setBase(SystemClock.elapsedRealtime());
+                       chronometer.start();
+                       start=false;
+                   }else{
+                       chronometer.setBase(base);
+                       chronometer.start();
+
+                   }
+
+                }
+                else if(event.getAction()==MotionEvent.ACTION_MOVE){
+                   if(paintView.isOutIndex()==true){
+                       finishButton.setText("TRACED OUT");
+                       finishButton.setBackgroundColor(Color.RED);
+                   } else{
+                       finishButton.setBackgroundColor(Color.parseColor("#D81B60"));
+                       finishButton.setText("CONTINUE");
+                   }
+                }
+                else if(event.getAction()==MotionEvent.ACTION_UP){
+                        finishButton.setText("CLICK HERE IF FINISHED");
+                        base=chronometer.getBase();
+                        chronometer.setBase(base);
+                        chronometer.stop();
+                }
+
+
+                return false;
+            }
+        });
+
+
+    }
+
+    private void showfinalDialogue(int marks, final PaintView paintView) {
+        // Create Alert using Builder
+        if(marks>=paintView.getSCORE()) {
+
+            CFAlertDialog.Builder builder = new CFAlertDialog.Builder(this)
+                    .setDialogStyle(CFAlertDialog.CFAlertStyle.BOTTOM_SHEET)
+                    .setTitle("Success. Congrats you hit the score")
+                    .setIcon(R.drawable.ic_check_circle_black_24dp)
+                    .addButton("Try your self", -1, -1, CFAlertDialog.CFAlertActionStyle.POSITIVE,
+                            CFAlertDialog.CFAlertActionAlignment.END, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            });
+
+            builder.show();
+        }else{
+            CFAlertDialog.Builder builder = new CFAlertDialog.Builder(this)
+                    .setDialogStyle(CFAlertDialog.CFAlertStyle.BOTTOM_SHEET)
+                    .setTitle("Failed. Score not satisfactory, please try again.")
+                    .setIcon(R.drawable.ic_cancel_black_24dp)
+                    .addButton("Redo", -1, -1, CFAlertDialog.CFAlertActionStyle.NEGATIVE,
+                            CFAlertDialog.CFAlertActionAlignment.END, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    recreate();
+                                }
+                            });
+
+            builder.show();
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/concentriccircledraw/PaintView.java
@@ -0,0 +1,168 @@
+package com.example.concentriccircledraw;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class PaintView extends android.support.v7.widget.AppCompatImageView {
+
+    public ViewGroup.LayoutParams params;
+    private Path path=new Path();
+    private Paint brush=new Paint();
+    private int SCORE=28;
+    private Bitmap bitmap;
+    private int touched=0;
+    private int total=0;
+    private boolean outIndex=false;
+    private ArrayList<Integer> pixels=new ArrayList<>();
+    private int totalCoordinates=0;
+
+    public ArrayList<Integer> getPixels() {
+        return pixels;
+    }
+
+    public void setPixels(ArrayList<Integer> pixels) {
+        this.pixels = pixels;
+    }
+
+    public int getSCORE() {
+        return SCORE;
+    }
+
+    public void setSCORE(int SCORE) {
+        this.SCORE = SCORE;
+    }
+
+    public PaintView(Context context) {
+        super(context);
+
+        brush.setAntiAlias(true);
+        brush.setColor(Color.MAGENTA);
+        brush.setStyle(Paint.Style.STROKE);
+        brush.setStrokeJoin(Paint.Join.ROUND);
+        brush.setStrokeWidth(6f);
+
+        this.setDrawingCacheEnabled(true);
+        this.buildDrawingCache(true);
+        params=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+
+
+    }
+
+    public PaintView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        brush.setAntiAlias(true);
+        brush.setColor(Color.MAGENTA);
+        brush.setStyle(Paint.Style.STROKE);
+        brush.setStrokeJoin(Paint.Join.ROUND);
+        brush.setStrokeWidth(6f);
+
+        this.setBackground(context.getDrawable(R.drawable.circle));
+        this.setDrawingCacheEnabled(true);
+        this.buildDrawingCache(true);
+
+    }
+
+    public PaintView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        brush.setAntiAlias(true);
+        brush.setColor(Color.MAGENTA);
+        brush.setStyle(Paint.Style.STROKE);
+        brush.setStrokeJoin(Paint.Join.ROUND);
+        brush.setStrokeWidth(6f);
+
+        this.setBackground(context.getDrawable(R.drawable.circle));
+        this.setDrawingCacheEnabled(true);
+        this.buildDrawingCache(true);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX=event.getX();
+        float pointY=event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(pointX,pointY);
+                return true;
+
+            case MotionEvent.ACTION_MOVE:
+                bitmap=this.getDrawingCache();
+
+                if((int)event.getX()>0&&(int)event.getY()>0&&(int)event.getY()<bitmap.getHeight()
+                &&(int)event.getX()<bitmap.getWidth()) {
+
+                    int pixel = bitmap.getPixel((int) event.getX(), (int) event.getY());
+
+                    pixels.add((int) event.getX()+(int) event.getY());
+
+                    int r = Color.red(pixel);
+                    int g = Color.green(pixel);
+                    int b = Color.blue(pixel);
+
+                    total++;
+
+                    if(r==0&&g==0&&b==0){
+                        outIndex=false;
+                        touched++;
+
+                    }
+
+                    else if(r==255&&g==255&&b==255){
+                        outIndex=true;
+                    }
+                    else
+                        {
+                        }
+                    path.lineTo(pointX, pointY);
+                }
+                break;
+            default:
+                return false;
+
+        }
+        postInvalidate();
+        return false;
+    }
+
+    public boolean isOutIndex() {
+        return outIndex;
+    }
+
+    public void setOutIndex(boolean outIndex) {
+        this.outIndex = outIndex;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+          canvas.drawPath(path,brush);
+         }
+
+    public int getTouched() {
+        return touched;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void setTouched(int touched) {
+        this.touched = touched;
+    }
+}
